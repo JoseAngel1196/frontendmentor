dcf1d3327cc4630dbfcc8fd615a0eb1223cfa74d
Jose Angel Hidalgo Rosa <joseangel1196@hotmail.com>
2020-04-20 13:39:46 -0400

    ping-coming-soon-page-master

---
+++ ping-coming-soon-page-master/index.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = form["email"];
+  const emailValue = email.value;
+  const small = form.querySelector("small");
+
+  if (!emailValue) {
+    email.classList.add("error");
+    small.innerText = "Email field cannot be blank!";
+    small.style.display = "inline-block";
+  } else if (!isValidEmail(emailValue)) {
+    email.classList.add("error");
+    small.innerText = "Email is invalid!";
+    small.style.display = "inline-block";
+  } else {
+    email.classList.remove("error");
+    small.innerText = "";
+    small.style.display = "none";
+  }
+});
+
+function isValidEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
